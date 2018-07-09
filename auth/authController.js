4c32de5a28a91e6bc4a4b58d6251c8a4da5cfc14
subhamgupta26 <subhamgupta026@gmail.com>
2018-07-09 23:03:06 +0530

    basic security implemented

---
+++ auth/authController.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../config');
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+var User = require('../user/User');
--- routes/public.js
+++ routes/public.js
@@ -1,22 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../config');
+
 
 var User = require('../models/user');
 
 /* GET users listing. */
 router.post('/signup', function(req, res, next) {
 
-  //res.send('respond with a resource');
+  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
       User.create({
             name : req.body.name,
             email : req.body.email,
-            password : req.body.password
+            password : hashedPassword
         }, 
         function (err, user) {
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            res.status(200).send(user);
+
+             var token = jwt.sign({ id: user._id }, config.secret, {
+      expiresIn: 86400 // expires in 24 hours
+    });
+            res.status(200).send({ auth: true, token: token });
         });
 });
 
@@ -33,11 +41,17 @@ router.post('/login', function(req, res, next) {
           return res.status(404).send("User doesnot exist.");
         }
         //console.log(currentUser);
-        if(currentUser.password !== req.body.password ){
-          return res.status(401).send("Username or password incorrect");
-        }
+        // if(currentUser.password !== req.body.password ){
+        //   return res.status(401).send("Username or password incorrect");
+        // }
 
-        res.status(200).send({message:"Login successful!!"});
+            var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+    var token = jwt.sign({ id: user._id }, config.secret, {
+      expiresIn: 86400 // expires in 24 hours
+    });
+
+        res.status(200).send({ auth: true, token: token });
     });
 });
 
