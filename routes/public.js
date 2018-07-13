d7a47f29955e45d083c7b7257811101286a4adfe
Subham Gupta <subham_gupta@persistent.co.in>
2018-07-13 11:41:53 +0530

    removed unnecessary comments

--- routes/public.js
+++ routes/public.js
@@ -21,19 +21,6 @@ router.post('/signup', function (req, res, next) {
   
   var hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
-  // User.create({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: hashedPassword
-  // },
-  //   function (err, user) {
-  //     if (err) return res.status(500).send({ message: "There was a problem adding the information to the database." });
-
-  //     var token = jwt.sign({ id: user._id }, config.secret, {
-  //       expiresIn: 86400 // expires in 24 hours
-  //     });
-  //     res.status(200).send({ auth: true, token: token });
-  //   });
 
   userService.createUser(req.body.email, req.body.name, hashedPassword,function(err, user){
       if (err) {
@@ -51,24 +38,6 @@ router.post('/signup', function (req, res, next) {
 
 router.post('/login', function (req, res, next) {
 
-  //res.send('respond with a resource');
-
-  //     User.findOne({email : req.body.email}, function (err, currentUser ) {
-  //     if (err) {
-  //       return res.status(500).send({message:"There was a problem finding the user."});
-  //     }
-  //     if(!currentUser){
-  //       return res.status(404).send({message:"User doesnot exist."});
-  //     }
-
-  //         var passwordIsValid = bcrypt.compareSync(req.body.password, currentUser.password);
-  // if (!passwordIsValid) return res.status(401).send({ auth: false, token: null,message: 'Username and password doesnt match' });
-  // var token = jwt.sign({ id: currentUser._id }, config.secret, {
-  //   expiresIn: 86400 // expires in 24 hours
-  // });
-
-  //     res.status(200).send({ auth: true, token: token });
-  // });
 
   userService.getUserByEmail(req.body.email, function (err, currentUser) {
     if (err) {
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-// var User = require('../models/user');
-
-// var Product = require('../models/product');
-
 var _ = require('underscore');
 
 var userService = new (require('../service/userService.js')).userService();
 var productService = new (require('../service/productService.js')).productService();
 
-// var VerifyToken = require('../auth/VerifyToken');
 
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
-  // User.find({}, function(err, users) {
-  //   if (err){
-  //     console.log(err);
-  //     return res.status(500).send('There was a problem finding the users.');
-  //   }
-  //   res.status(200).send(users);
-  // });
 
   userService.getUsers(function(err, users) {
     if (err) {
@@ -33,17 +21,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:userId/cart', function(req, res, next) {
-  // User.findById(req.params.userId, function(err, user) {
-  //   if (err)
-  //     return res.status(500).send('There was a problem finding the user.');
-  //   if (!user) return res.status(404).send('No user found.');
-  //   Product.find({ _id: { $in: user.products } }, function(err, products) {
-  //     if (err)
-  //       return res.status(500).send('There was a problem finding the product.');
-  //     if (!products) return res.status(404).send('No product found.');
-  //     res.status(200).send({'response':products});
-  //   });
-  // });
+
   userService.getUserById(req.params.userId, function(err, user) {
     if (err) {
       return res.status(500).send('There was a problem finding the user.');
@@ -65,31 +43,6 @@ router.get('/:userId/cart', function(req, res, next) {
 
 router.put('/:userId/updatecart', function(req, res, next) {
   console.log('inside user update cart');
-  // User.findById(req.params.userId, function(err, user) {
-  //   if (err) {
-  //     console.log(err);
-  //     return res
-  //       .status(500)
-  //       .send({ message: 'There was a problem finding the user.' });
-  //   }
-  //   if (!user) return res.status(404).send({ message: 'No user found.' });
-  //   if (user.products.indexOf(req.body._id) > -1) {
-  //     return res
-  //       .status(400)
-  //       .send({ message: 'This product is already in cart.' });
-  //   }
-  //   user.products.push(req.body._id);
-  //   User.update({ _id: user.id }, user, function(err, user) {
-  //     if (err) {
-  //       console.log(err);
-  //       return res
-  //         .status(500)
-  //         .send({ message: 'There was a problem updating the cart.' });
-  //     }
-  //     if (!user) return res.status(404).send({ message: 'Updation failed' });
-  //     res.status(200).send({ message: 'Update Successful!!' });
-  //   });
-  // });
 
   userService.getUserById(req.params.userId, function(err,user){
     if (err) {
@@ -125,17 +78,6 @@ router.put('/:userId/updatecart', function(req, res, next) {
 
 
 router.get('/current', function(req, res, next) {
-  // console.log(req.userId);
-  // User.findById(req.userId, function(err, user) {
-  //   if (err) {
-  //     console.log(err);
-  //     return res
-  //       .status(500)
-  //       .send({ message: 'There was a problem finding the user.' });
-  //   }
-  //   if (!user) return res.status(404).send({ message: 'No user found.' });
-  //   res.status(200).send(user);
-  // });
 
   userService.getUserById(req.userId, function(err, user){
         if (err) {
